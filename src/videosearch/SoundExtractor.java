3f7fdcc9fd6413a81f5204929c6a053e20a67b02
shawntsai <b96501027@gmail.com>
2018-04-18 14:01:51 -0700

    add base frequecy extractor and dominant frequency extractor

--- src/videosearch/SoundExtractor.java
+++ src/videosearch/SoundExtractor.java
@@ -1,36 +1,33 @@
 package videosearch;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.SilenceDetector;
+import be.tarsos.dsp.SpectralPeakProcessor;
 import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
-import be.tarsos.dsp.AudioProcessor; 
-import be.tarsos.dsp.SilenceDetector; 
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchDetector;
+import be.tarsos.dsp.pitch.PitchProcessor;
+import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm;
+import be.tarsos.dsp.SpectralPeakProcessor; 
+import be.tarsos.dsp.SpectralPeakProcessor.SpectralPeak; 
 
 
 
 public class SoundExtractor {
-//    private final static int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
-//    InputStream waveStream;
-    
-//	public SoundExtractor(InputStream waveStream) {
-//		this.waveStream = waveStream;
-//		
-//	}
 	List<Feature> features = new ArrayList<>();
-	
+	int audioBufferSize;
 	public SoundExtractor(File audioFile, int audioBufferSize, int bufferOverlap) {
+		this.audioBufferSize = audioBufferSize;
 		try {
 			AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(audioFile, audioBufferSize, bufferOverlap);
 			RootMeanSquareExtractor rmsExtractor = new RootMeanSquareExtractor();
@@ -43,12 +40,101 @@ public class SoundExtractor {
 			features.add(new Feature(spe.name, dbspl));
 //			for (Double x: dbspl) System.out.println(x);
 			
+			
+			dispatcher = AudioDispatcherFactory.fromFile(audioFile, audioBufferSize, bufferOverlap);
+			SoundBaseFrequencyExtractor sb = new SoundBaseFrequencyExtractor();
+			List<Double> sbf = sb.run(dispatcher);
+			System.out.println("sbf frequencies length");
+			System.out.println(sbf.size());
+			
+			
 		} catch (UnsupportedAudioFileException exception) {
 			System.out.println("not support this file format");
 		} catch (IOException exception) {
 			System.out.println("file not correnct");
 		}		
 	}
+	
+	private class SoundBaseFrequencyExtractor {
+		String name = "base frequency";
+		
+		public List<Double> run(AudioDispatcher dispatcher) {
+			List<Double> frequencies = new ArrayList<>();
+			PitchEstimationAlgorithm algo = PitchEstimationAlgorithm.FFT_YIN; 
+			PitchDetector detector = algo.getDetector(44100, audioBufferSize);			
+			dispatcher.addAudioProcessor(new AudioProcessor() {
+
+				@Override
+				public boolean process(AudioEvent audioEvent) {
+					// TODO Auto-generated method stub
+					float[] audioFloatBuffer = audioEvent.getFloatBuffer(); 
+					PitchDetectionResult result = detector.getPitch(audioFloatBuffer);
+//					result.getPitch();
+//					result.getProbability();
+					frequencies.add((double)result.getPitch());
+					return true;
+				}
+
+				@Override
+				public void processingFinished() {}				
+			});			
+			dispatcher.run();
+			return frequencies;
+		}
+		@Override
+		public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
+			   double timeStamp = audioEvent.getTimeStamp(); 
+			   double pitch = pitchDetectionResult.getPitch(); 
+			   float probability = pitchDetectionResult.getProbability(); 
+			   frequencies.add(pitch);
+			   System.out.println(timeStamp+","+pitch+","+probability); 
+		}
+	}
+	
+	private class SoundDominantFrequencyExtractor {
+		String name = "dominant frequency";
+		public List<Double> run(AudioDispatcher dispatcher) {
+						
+			List<Double> frequencies = new ArrayList<>();
+//			FFT fft = new FFT(audioBufferSize);
+//			float[] amplitudes = new float[audioBufferSize / 2];
+			SpectralPeakProcessor sp = new SpectralPeakProcessor(audioBufferSize, 0,44100);
+			dispatcher.addAudioProcessor(sp);
+			dispatcher.addAudioProcessor(new AudioProcessor() {
+				@Override
+				public boolean process(AudioEvent audioEvent) {
+//					float[] audioFloatBuffer = audioEvent.getFloatBuffer();
+//					float[] transformBuffer = new float[audioBufferSize*2];
+//					System.arraycopy(audioFloatBuffer, 0, transformBuffer, 0, audioFloatBuffer.length);
+//					fft.forwardTransform(transformBuffer);
+//					fft.modulus(transformBuffer, amplitudes);					
+					frequencies.add(findPeak(sp.getMagnitudes(), sp.getFrequencyEstimates());
+					return true;
+				}
+
+				@Override
+				public void processingFinished() {
+					// TODO Auto-generated method stub					
+				}
+			
+			});
+			dispatcher.run();
+			return frequencies;
+		}
+		private double findPeak(float[] fs, float[] fs2) {
+			assert(fs.length == fs2.length);
+			double max = Double.MIN_VALUE;
+			double dominantFrequency = 0;
+			for (int i = 0; i < fs.length; i++) {
+				if (fs[i] > max) {
+					max = fs[i];
+					dominantFrequency = fs2[i];
+				}
+			}
+			return dominantFrequency;
+		}
+	}
+	
 	
 	private class SoundPressureLevelExtractor {
 		String name = "sound pressure level";
@@ -63,7 +149,7 @@ public class SoundExtractor {
 				@Override
 				public boolean process(AudioEvent audioEvent) {
 					dbspl.add(sd.currentSPL());					
-					return false;
+					return true;
 				}
 
 				@Override
