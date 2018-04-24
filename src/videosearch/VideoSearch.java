82e5bfc1cbcdf6ffdd43e9d6041872ae49267bba
cjshieh <chingjshieh@gmail.com>
2018-04-23 17:02:08 -0700

    connect to UI

--- src/videosearch/VideoSearch.java
+++ src/videosearch/VideoSearch.java
@@ -1,53 +1,113 @@
 package videosearch;
+import ui.VideoSearchUI;
+import ui.model.AllResultModel;
+
 import java.io.File;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VideoSearch {
      
 	static List<Video> videos = new ArrayList<>();
-	
-	
-	public static void main(String[] args) {		
+//	static Map<String, Rank> allResult = new HashMap<>();
+	private final static String queryFolder = "query";
+	private final static String dbName = "database_videos";
+	public Video queryVideo;
+	private AllResultModel storedResult = new AllResultModel();
+
+	public void loadDataBase() {
+	    try {
+//            String queryFolder = "query";
+//            String dbName = "database_videos";
+            File databaseFolder = new File(dbName);
+            for (File fileOrFolder: databaseFolder.listFiles()) {
+                if (fileOrFolder.isDirectory()) {
+                    String q = fileOrFolder.getName();
+                    String audioFilePath = dbName + "/" + q + "/" + q + ".wav";
+                    videos.add(new Video(q, audioFilePath, dbName + "/" + q));
+                }
+            }
+            for (Video video: videos) {
+                video.extractFeature();
+            }
+        }catch (Exception e) {
+            System.out.println("something wrong");
+            e.printStackTrace();
+        }
+    }
+
+    public void loadQuery(String q) {
 		try {
-			
-			String queryFolder = "query";
-			String dbName = "database_videos";
-			File databaseFolder = new File(dbName);
-			for (File fileOrFolder: databaseFolder.listFiles()) {
-				if (fileOrFolder.isDirectory()) {
-					String q = fileOrFolder.getName();
-					String audioFilePath = dbName + "/" + q + "/" + q + ".wav";
-					videos.add(new Video(q, audioFilePath, dbName + "/" + q));					
-				}
-			}
-			for (Video video: videos) {
-				video.extractFeature();
-			}
-			String q = "first";
-			
-			Video queryVideo = new Video(q, queryFolder + "/" + q + "/" + q + ".wav", queryFolder + "/" + q);
+			this.queryVideo = new Video(q, queryFolder + "/" + q + "/" + q + ".wav", queryFolder + "/" + q);
 			queryVideo.extractFeature();
+		}catch (Exception e) {
+			System.out.println("something wrong");
+			e.printStackTrace();
+		}
+	}
+
+    public AllResultModel compareDB() {
+//		AllResultModel storedResult = new AllResultModel();
+        for (String method: queryVideo.features.keySet()) {
+            Rank ranker = new Rank(videos, method);
+            ranker.compare(queryVideo.features.get(method));
+            storedResult.setResult(method, ranker);
+        }
+        return storedResult;
+	}
+
+	public AllResultModel getStoredResult() {
+		return storedResult;
+	}
+
+	public void compareDBviaMethod(String method) {
+		Rank ranker = new Rank(videos, method);
+		ranker.compare(queryVideo.features.get(method));
+		storedResult.setResult(method, ranker);
+	}
+
+
+	
+//	public static void main(String[] args) {
+//		try {
+//
+//			String q = "first";
 			
-			for (String method: queryVideo.features.keySet()) {
+
+			
+
 //				System.out.println("rank this");
 //				System.out.println(method);
-				Rank ranker = new Rank(videos, method);
-				ranker.compare(queryVideo.features.get(method));
-				int i = 1;
-				while (ranker.scores.isEmpty() == false) {
-					Score s = ranker.scores.poll();
-					System.out.println(i++);
-					System.out.println(s.videoName);			
-//					System.out.println(s.score);
-					System.out.println(s.ratio);
-					System.out.println("start frame is ");
-					System.out.println((int) (600 * s.ratio));
-					System.out.println(s.getDistribution());
-				}
-			}
-			
+
+
+//				int i = 1;
+//				while (ranker.scores.isEmpty() == false) {
+//					Score s = ranker.scores.poll();
+//					System.out.println(i++);
+//					System.out.println(s.videoName);
+////					System.out.println(s.score);
+//					System.out.println(s.ratio);
+//					System.out.println("start frame is ");
+//					System.out.println((int) (600 * s.ratio));
+//					System.out.println(s.getDistribution());
+//				}
+//			}
+//			System.out.println("======");
+//			Rank motion = allResult.get("rgb image motion");
+//			int i = 1;
+//			while(!motion.scores.isEmpty()) {
+//				Score s = motion.scores.poll();
+//					System.out.println(i++);
+//					System.out.println(s.videoName);
+////					System.out.println(s.score);
+//					System.out.println(s.ratio);
+//					System.out.println("start frame is ");
+//					System.out.println((int) (600 * s.ratio));
+//					System.out.println(s.getDistribution());
+//			}
 //			String method1 = "root mean square";
 //			Rank ranker = new Rank(videos, method1);
 //			ranker.compare(queryVideo.features.get(method1));
@@ -71,18 +131,15 @@ public class VideoSearch {
 //			playSound.play();
 
 		
-		} 
+//		}
 //		catch (FileNotFoundException e) {
 //			System.out.println("can not find file");
 //		}	
 //		catch (PlayWaveException e) {
 //			System.out.println("can not play");
 //		}
-		catch (Exception e) {
-			System.out.println("something wrong");
-			e.printStackTrace();
-		}
+
 		
-	}
+//	}
 
 }
